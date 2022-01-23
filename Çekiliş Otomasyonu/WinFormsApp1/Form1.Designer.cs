c14ea68d429da9b4b90ea23d7118e53f09b68bd8
tolgahan54 <93204585+tolgahan54@users.noreply.github.com>
2022-01-23 23:24:48 +0300

    Add files via upload

---
+++ Çekiliş Otomasyonu/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 24);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(150, 244);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(449, 24);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(150, 244);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(638, 24);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(150, 244);
+            this.listBox3.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 288);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 27);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "EKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(168, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(275, 34);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "ÇEKİLİŞ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(168, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(275, 27);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(168, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(275, 34);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "OTOMATİK DAĞILIM";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(222, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Çekiliş Kazanan Sayısı";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(447, 321);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(341, 92);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "DB den Veriyi Al";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Çekiliş Otomasyonu/WinFormsApp1/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                listBox1.Items.Add(textBox1.Text);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int sayi = Int32.Parse(textBox2.Text);
+                Random Rnd = new Random();
+                for (int i = 1; i <= sayi; i++)
+                {
+                    int tutulan = Rnd.Next(0, listBox1.Items.Count);
+                    listBox2.Items.Add(listBox1.Items[tutulan]);
+                    listBox1.Items.RemoveAt(tutulan);
+                }
+            }
+
+            catch
+            {
+                {
+                    MessageBox.Show("Bilgileri kontrol edin.");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int kisisayisi = listBox1.Items.Count;
+            for (int i = 1; i <=kisisayisi; i++)
+            {
+                int tutulan = rnd.Next(0, listBox1.Items.Count);
+                if (i % 2 == 1)
+                {
+                    listBox2.Items.Add(listBox1.Items[tutulan]);
+                    listBox1.Items.RemoveAt(tutulan);
+                }
+                else
+                {
+                    listBox3.Items.Add(listBox1.Items[tutulan]);
+                    listBox1.Items.RemoveAt(tutulan);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=TOLGA\\WINCCPLUSMIG2014;Initial Catalog=AdventureWorks2014;Integrated Security=True");
+
+                SqlCommand cmd = new SqlCommand("select name from HumanResources.Department where DepartmentID=1 ", con);
+                SqlCommand cmd2 = new SqlCommand("select name from HumanResources.Department where DepartmentID=2 ", con);
+                SqlCommand cmd3 = new SqlCommand("select name from HumanResources.Department where DepartmentID=3 ", con);
+                SqlCommand cmd4 = new SqlCommand("select name from HumanResources.Department where DepartmentID=4 ", con);
+                SqlCommand cmd5 = new SqlCommand("select name from HumanResources.Department where DepartmentID=5 ", con);
+                SqlCommand cmd6 = new SqlCommand("select name from HumanResources.Department where DepartmentID=6 ", con);
+                SqlCommand cmd7 = new SqlCommand("select name from HumanResources.Department where DepartmentID=7 ", con);
+                SqlCommand cmd8 = new SqlCommand("select name from HumanResources.Department where DepartmentID=8 ", con);
+                SqlCommand cmd9 = new SqlCommand("select name from HumanResources.Department where DepartmentID=9 ", con);
+                SqlCommand cmd10 = new SqlCommand("select name from HumanResources.Department where DepartmentID=10 ", con);
+                SqlCommand cmd11 = new SqlCommand("select name from HumanResources.Department where DepartmentID=11 ", con);
+                con.Open();
+                    listBox1.Items.Add(cmd.ExecuteScalar());
+                    listBox1.Items.Add(cmd2.ExecuteScalar());
+                    listBox1.Items.Add(cmd3.ExecuteScalar());
+                    listBox1.Items.Add(cmd4.ExecuteScalar());
+                    listBox1.Items.Add(cmd5.ExecuteScalar());
+                    listBox1.Items.Add(cmd6.ExecuteScalar());
+                    listBox1.Items.Add(cmd7.ExecuteScalar());
+                    listBox1.Items.Add(cmd8.ExecuteScalar());
+                    listBox1.Items.Add(cmd9.ExecuteScalar());
+                    listBox1.Items.Add(cmd10.ExecuteScalar());
+                    listBox1.Items.Add(cmd11.ExecuteScalar());
+                con.Close();
+            
+        }
+
+    }
+}
